ff40d3a00912d87e3b5385599f85e79de8b71b5b
Guilherme <gosantos@ufrgs.br>
2017-03-24 15:52:39 -0300

    added answers bundle

--- best_tv_series.rb
+++ best_tv_series.rb
@@ -22,48 +22,43 @@ series.push Series.new(SeriesBundle::SILICON_VALLEY_ID, SeriesBundle::SILICON_VA
 questions = []
 
 question = Question.new(QuestionsBundle::QUESTION_1_ID, QuestionsBundle::QUESTION_1_TITLE)
-question.add_answer Answer.new('a', "Acorda cedo e come frutas cortadas metodicamente.")
-question.add_answer Answer.new('b', "Sai da cama com o despertador e se prepara para a batalha da semana.")
-question.add_answer Answer.new('c', "Só consegue lembrar do seu nome depois do café.")
-question.add_answer Answer.new('d', "Levanta e faz café pra todos da casa.")
-question.add_answer Answer.new('e', "Passa o café e conserta um erro no HTML.")
-
+question.add_answer Answer.new(AnswersBundle::QUESTION_1_ANSWER_A_SERIE_ID, AnswersBundle::QUESTION_1_ANSWER_A_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_1_ANSWER_B_SERIE_ID, AnswersBundle::QUESTION_1_ANSWER_B_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_1_ANSWER_C_SERIE_ID, AnswersBundle::QUESTION_1_ANSWER_C_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_1_ANSWER_D_SERIE_ID, AnswersBundle::QUESTION_1_ANSWER_D_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_1_ANSWER_E_SERIE_ID, AnswersBundle::QUESTION_1_ANSWER_E_TITLE)
 questions.push question
 
 question = Question.new(QuestionsBundle::QUESTION_2_ID, QuestionsBundle::QUESTION_2_TITLE)
-question.add_answer Answer.new('a', "Ela vai atrapalhar seu horário. Oculte o corpo.")
-question.add_answer Answer.new('b', "Levanta a senhora e jura protegê-la com sua vida.")
-question.add_answer Answer.new('c', "Ajuda-a, mas questiona sua real identidade.")
-question.add_answer Answer.new('d', "Oferece para caminharem juntos até um destino em comum.")
-question.add_answer Answer.new('e', "Testa se ela roda bem no Firefox. Não roda.")
-
+question.add_answer Answer.new(AnswersBundle::QUESTION_2_ANSWER_A_SERIE_ID, AnswersBundle::QUESTION_2_ANSWER_A_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_2_ANSWER_B_SERIE_ID, AnswersBundle::QUESTION_2_ANSWER_B_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_2_ANSWER_C_SERIE_ID, AnswersBundle::QUESTION_2_ANSWER_C_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_2_ANSWER_D_SERIE_ID, AnswersBundle::QUESTION_2_ANSWER_D_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_2_ANSWER_E_SERIE_ID, AnswersBundle::QUESTION_2_ANSWER_E_TITLE)
 questions.push question
 
 question = Question.new(QuestionsBundle::QUESTION_3_ID, QuestionsBundle::QUESTION_3_TITLE)
-question.add_answer Answer.new('a', "Convence parte das pessoas a esperarem o próximo.")
-question.add_answer Answer.new('b', "Ignora as pessoas no elevador e entra de qualquer forma.")
-question.add_answer Answer.new('c', "Você questiona a realidade, as coisas e tudo mais. Sobe de escada.")
-question.add_answer Answer.new('d', "Com uma leve intimidação passivo-agressiva, encontra um lugar no elevador.")
-question.add_answer Answer.new('e', "Cria um app que mostra a lotação do elevador. Vende o app e fica milionário.")
-
+question.add_answer Answer.new(AnswersBundle::QUESTION_3_ANSWER_A_SERIE_ID, AnswersBundle::QUESTION_3_ANSWER_A_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_3_ANSWER_B_SERIE_ID, AnswersBundle::QUESTION_3_ANSWER_B_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_3_ANSWER_C_SERIE_ID, AnswersBundle::QUESTION_3_ANSWER_C_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_3_ANSWER_D_SERIE_ID, AnswersBundle::QUESTION_3_ANSWER_D_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_3_ANSWER_E_SERIE_ID, AnswersBundle::QUESTION_3_ANSWER_E_TITLE)
 questions.push question
 
 question = Question.new(QuestionsBundle::QUESTION_4_ID, QuestionsBundle::QUESTION_4_TITLE)
-question.add_answer Answer.new('a', "Fala sobre a política, eleições, como tudo é um absurdo.")
-question.add_answer Answer.new('b', "Larga uma frase polêmica e vê uma pequena guerra se formar.")
-question.add_answer Answer.new('c', "Puxa um assunto e te lembram que já foi discutido semana passada.")
-question.add_answer Answer.new('d', "Sugere que os colegas trabalhem na ideia de um novo projeto.")
-question.add_answer Answer.new('e', "Desabafa sobre como odeia PHP. Todo mundo na sala adora PHP.")
-
+question.add_answer Answer.new(AnswersBundle::QUESTION_4_ANSWER_A_SERIE_ID, AnswersBundle::QUESTION_4_ANSWER_A_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_4_ANSWER_B_SERIE_ID, AnswersBundle::QUESTION_4_ANSWER_B_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_4_ANSWER_C_SERIE_ID, AnswersBundle::QUESTION_4_ANSWER_C_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_4_ANSWER_D_SERIE_ID, AnswersBundle::QUESTION_4_ANSWER_D_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_4_ANSWER_E_SERIE_ID, AnswersBundle::QUESTION_4_ANSWER_E_TITLE)
 questions.push question
 
 question = Question.new(QuestionsBundle::QUESTION_5_ID, QuestionsBundle::QUESTION_5_TITLE)
-question.add_answer Answer.new('a', "Vou chamar aqui o meu Uber.")
-question.add_answer Answer.new('b', "Pegarei o bus junto com o resto do povo.")
-question.add_answer Answer.new('c', "No ponto de ônibus mais uma vez, espero não errar a linha de novo.")
-question.add_answer Answer.new('d', "Vou de carro, mas ofereço uma carona para os colegas.")
-question.add_answer Answer.new('e', "Acho que descobri uma forma de fazer aquela senhora rodar no Firefox.")
-
+question.add_answer Answer.new(AnswersBundle::QUESTION_5_ANSWER_A_SERIE_ID, AnswersBundle::QUESTION_5_ANSWER_A_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_5_ANSWER_B_SERIE_ID, AnswersBundle::QUESTION_5_ANSWER_B_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_5_ANSWER_C_SERIE_ID, AnswersBundle::QUESTION_5_ANSWER_C_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_5_ANSWER_D_SERIE_ID, AnswersBundle::QUESTION_5_ANSWER_D_TITLE)
+question.add_answer Answer.new(AnswersBundle::QUESTION_5_ANSWER_E_SERIE_ID, AnswersBundle::QUESTION_5_ANSWER_E_TITLE)
 questions.push question
 
 ui = UserInterface.new
---
+++ lib/answers_bundle.rb
@@ -0,0 +1,56 @@
+module AnswersBundle
+  QUESTION_1_ANSWER_A_SERIE_ID = 'a'
+  QUESTION_1_ANSWER_A_TITLE = "Acorda cedo e come frutas cortadas metodicamente."
+  QUESTION_1_ANSWER_B_SERIE_ID = 'b'
+  QUESTION_1_ANSWER_B_TITLE = "Sai da cama com o despertador e se prepara para a batalha da semana."
+  QUESTION_1_ANSWER_C_SERIE_ID = 'c'
+  QUESTION_1_ANSWER_C_TITLE = "Só consegue lembrar do seu nome depois do café."
+  QUESTION_1_ANSWER_D_SERIE_ID = 'd'
+  QUESTION_1_ANSWER_D_TITLE = "Levanta e faz café pra todos da casa."
+  QUESTION_1_ANSWER_E_SERIE_ID = 'e'
+  QUESTION_1_ANSWER_E_TITLE = "Passa o café e conserta um erro no HTML."
+
+  QUESTION_2_ANSWER_A_SERIE_ID = 'a'
+  QUESTION_2_ANSWER_A_TITLE = "Ela vai atrapalhar seu horário. Oculte o corpo."
+  QUESTION_2_ANSWER_B_SERIE_ID = 'b'
+  QUESTION_2_ANSWER_B_TITLE = "Levanta a senhora e jura protegê-la com sua vida."
+  QUESTION_2_ANSWER_C_SERIE_ID = 'c'
+  QUESTION_2_ANSWER_C_TITLE = "Ajuda-a, mas questiona sua real identidade."
+  QUESTION_2_ANSWER_D_SERIE_ID = 'd'
+  QUESTION_2_ANSWER_D_TITLE = "Oferece para caminharem juntos até um destino em comum."
+  QUESTION_2_ANSWER_E_SERIE_ID = 'e'
+  QUESTION_2_ANSWER_E_TITLE = "Testa se ela roda bem no Firefox. Não roda."
+
+  QUESTION_3_ANSWER_A_SERIE_ID = 'a'
+  QUESTION_3_ANSWER_A_TITLE = "Convence parte das pessoas a esperarem o próximo."
+  QUESTION_3_ANSWER_B_SERIE_ID = 'b'
+  QUESTION_3_ANSWER_B_TITLE = "Ignora as pessoas no elevador e entra de qualquer forma."
+  QUESTION_3_ANSWER_C_SERIE_ID = 'c'
+  QUESTION_3_ANSWER_C_TITLE = "Você questiona a realidade, as coisas e tudo mais. Sobe de escada."
+  QUESTION_3_ANSWER_D_SERIE_ID = 'd'
+  QUESTION_3_ANSWER_D_TITLE = "Com uma leve intimidação passivo-agressiva, encontra um lugar no elevador."
+  QUESTION_3_ANSWER_E_SERIE_ID = 'e'
+  QUESTION_3_ANSWER_E_TITLE = "Cria um app que mostra a lotação do elevador. Vende o app e fica milionário."
+
+  QUESTION_4_ANSWER_A_SERIE_ID = 'a'
+  QUESTION_4_ANSWER_A_TITLE = "Fala sobre a política, eleições, como tudo é um absurdo."
+  QUESTION_4_ANSWER_B_SERIE_ID = 'b'
+  QUESTION_4_ANSWER_B_TITLE = "Larga uma frase polêmica e vê uma pequena guerra se formar."
+  QUESTION_4_ANSWER_C_SERIE_ID = 'c'
+  QUESTION_4_ANSWER_C_TITLE = "Puxa um assunto e te lembram que já foi discutido semana passada."
+  QUESTION_4_ANSWER_D_SERIE_ID = 'd'
+  QUESTION_4_ANSWER_D_TITLE = "Sugere que os colegas trabalhem na ideia de um novo projeto."
+  QUESTION_4_ANSWER_E_SERIE_ID = 'e'
+  QUESTION_4_ANSWER_E_TITLE = "Desabafa sobre como odeia PHP. Todo mundo na sala adora PHP."
+
+  QUESTION_5_ANSWER_A_SERIE_ID = 'a'
+  QUESTION_5_ANSWER_A_TITLE = "Vou chamar aqui o meu Uber."
+  QUESTION_5_ANSWER_B_SERIE_ID = 'b'
+  QUESTION_5_ANSWER_B_TITLE = "Pegarei o bus junto com o resto do povo."
+  QUESTION_5_ANSWER_C_SERIE_ID = 'c'
+  QUESTION_5_ANSWER_C_TITLE = "No ponto de ônibus mais uma vez, espero não errar a linha de novo."
+  QUESTION_5_ANSWER_D_SERIE_ID = 'd'
+  QUESTION_5_ANSWER_D_TITLE = "Vou de carro, mas ofereço uma carona para os colegas."
+  QUESTION_5_ANSWER_E_SERIE_ID = 'e'
+  QUESTION_5_ANSWER_E_TITLE = "Acho que descobri uma forma de fazer aquela senhora rodar no Firefox."
+end
