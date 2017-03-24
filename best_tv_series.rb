af9339e9f057054319bbc1d191c9d3e07bc61876
Guilherme <gosantos@ufrgs.br>
2017-03-24 15:37:43 -0300

    added questions bundle

--- best_tv_series.rb
+++ best_tv_series.rb
@@ -1,7 +1,10 @@
 require_relative "lib/series"
 require_relative "lib/series_bundle"
-require_relative "lib/answer"
+
 require_relative "lib/question"
+require_relative "lib/questions_bundle"
+
+require_relative "lib/answer"
 require_relative "lib/user_interface"
 require_relative "lib/answers_evaluator"
 
@@ -18,7 +21,7 @@ series.push Series.new(SeriesBundle::SILICON_VALLEY_ID, SeriesBundle::SILICON_VA
 
 questions = []
 
-question = Question.new(1, "De manhã, você: ")
+question = Question.new(QuestionsBundle::QUESTION_1_ID, QuestionsBundle::QUESTION_1_TITLE)
 question.add_answer Answer.new('a', "Acorda cedo e come frutas cortadas metodicamente.")
 question.add_answer Answer.new('b', "Sai da cama com o despertador e se prepara para a batalha da semana.")
 question.add_answer Answer.new('c', "Só consegue lembrar do seu nome depois do café.")
@@ -27,7 +30,7 @@ question.add_answer Answer.new('e', "Passa o café e conserta um erro no HTML.")
 
 questions.push question
 
-question = Question.new(2, "Indo para o trabalho você encontra uma senhora idosa caída na rua.")
+question = Question.new(QuestionsBundle::QUESTION_2_ID, QuestionsBundle::QUESTION_2_TITLE)
 question.add_answer Answer.new('a', "Ela vai atrapalhar seu horário. Oculte o corpo.")
 question.add_answer Answer.new('b', "Levanta a senhora e jura protegê-la com sua vida.")
 question.add_answer Answer.new('c', "Ajuda-a, mas questiona sua real identidade.")
@@ -36,7 +39,7 @@ question.add_answer Answer.new('e', "Testa se ela roda bem no Firefox. Não roda
 
 questions.push question
 
-question = Question.new(3, "Chega no prédio e o elevador está cheio.")
+question = Question.new(QuestionsBundle::QUESTION_3_ID, QuestionsBundle::QUESTION_3_TITLE)
 question.add_answer Answer.new('a', "Convence parte das pessoas a esperarem o próximo.")
 question.add_answer Answer.new('b', "Ignora as pessoas no elevador e entra de qualquer forma.")
 question.add_answer Answer.new('c', "Você questiona a realidade, as coisas e tudo mais. Sobe de escada.")
@@ -45,7 +48,7 @@ question.add_answer Answer.new('e', "Cria um app que mostra a lotação do eleva
 
 questions.push question
 
-question = Question.new(4, "Você chega no trabalho e as convenções sociais te obrigam a puxar assunto.")
+question = Question.new(QuestionsBundle::QUESTION_4_ID, QuestionsBundle::QUESTION_4_TITLE)
 question.add_answer Answer.new('a', "Fala sobre a política, eleições, como tudo é um absurdo.")
 question.add_answer Answer.new('b', "Larga uma frase polêmica e vê uma pequena guerra se formar.")
 question.add_answer Answer.new('c', "Puxa um assunto e te lembram que já foi discutido semana passada.")
@@ -54,7 +57,7 @@ question.add_answer Answer.new('e', "Desabafa sobre como odeia PHP. Todo mundo n
 
 questions.push question
 
-question = Question.new(5, "A pauta pegou o dia todo, mas você está indo para casa.")
+question = Question.new(QuestionsBundle::QUESTION_5_ID, QuestionsBundle::QUESTION_5_TITLE)
 question.add_answer Answer.new('a', "Vou chamar aqui o meu Uber.")
 question.add_answer Answer.new('b', "Pegarei o bus junto com o resto do povo.")
 question.add_answer Answer.new('c', "No ponto de ônibus mais uma vez, espero não errar a linha de novo.")
---
+++ lib/questions_bundle.rb
@@ -0,0 +1,16 @@
+module QuestionsBundle
+  QUESTION_1_ID = 1
+  QUESTION_1_TITLE = "De manhã, você: "
+
+  QUESTION_2_ID = 2
+  QUESTION_2_TITLE = "Indo para o trabalho você encontra uma senhora idosa caída na rua."
+
+  QUESTION_3_ID = 3
+  QUESTION_3_TITLE = "Chega no prédio e o elevador está cheio."
+
+  QUESTION_4_ID = 4
+  QUESTION_4_TITLE = "Você chega no trabalho e as convenções sociais te obrigam a puxar assunto."
+
+  QUESTION_5_ID = 5
+  QUESTION_5_TITLE = "A pauta pegou o dia todo, mas você está indo para casa."
+end
